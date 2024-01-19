2fe3aa08395793d573331fc4773f4bbdaaee4091
SREEMON KS <90373393+sreemonkavungal@users.noreply.github.com>
2024-01-19 11:16:35 +0530

    Update ContentSection.js

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -36,7 +36,7 @@ export default function ContentSection() {
             <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
                 <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
                     <div className="lg:pr-4">
-                        <div className="lg:max-w-lg">
+                        <div className="pt-7 lg:pt-0 lg:max-w-lg">
                             <p className="text-base font-semibold leading-7 text-indigo-600">Welcome To My Portfolio!</p>
                             <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"> Passionate Self-Taught Developer</h1>
                             <p className="mt-6 text-xl leading-8 text-gray-700">
