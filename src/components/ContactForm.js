cdeae1d15d7aff1850a54e06645cea604048f7fd
SREEMON KS <90373393+sreemonkavungal@users.noreply.github.com>
2024-01-23 15:47:42 +0530

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { Switch } from '@headlessui/react'
@@ -11,6 +11,63 @@ function classNames(...classes) {
 
 export default function ContactForm() {
   const [agreed, setAgreed] = useState(false)
+
+  const [formFields, setFormFields] = useState({
+    firstName: '',
+    lastName: '',
+    company: '',
+    email: '',
+    phoneNumber: '',
+    message: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormFields((prevFields) => ({ ...prevFields, [name]: value }));
+  };
+
+  const [submissionStatus, setSubmissionStatus] = useState(null);
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Fetch the Formspree endpoint with form data
+    const response = await fetch('https://formspree.io/f/xeqyeenr', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formFields),
+    });
+
+    if (response.ok) {
+      // Form submission was successful
+      alert('Form Submitted Successfully!');
+      setSubmissionStatus('success');
+      // Clear the form fields after successful submission
+    } else {
+      // Form submission failed
+      alert('Form Submission Failed. Check The Fields.');
+      setSubmissionStatus('failure');
+    }
+  };
+
+  useEffect(() => {
+    console.log('useEffect triggered:', submissionStatus);
+    if (submissionStatus === 'success') {
+      // Clear the form fields after successful submission
+      setFormFields({
+        firstName: '',
+        lastName: '',
+        company: '',
+        email: '',
+        phoneNumber: '',
+        message: '',
+      });
+    }
+  }, [submissionStatus]);
+console.log(submissionStatus)
+
 
   return (
     <div className=" bg-white px-6 py-20 sm:py-32 lg:px-8">
@@ -29,20 +86,26 @@ export default function ContactForm() {
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Let's Connect!</h2>
       </div>
-      <form action="https://formspree.io/f/mnqenqqr" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+      <form 
+      action="https://formspree.io/f/xeqyeenr" 
+      method="POST" 
+      className="mx-auto mt-16 max-w-xl sm:mt-20"
+      onSubmit={handleSubmit}
+      >
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
-            <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+            <label htmlFor="firstName" className="block text-sm font-semibold leading-6 text-gray-900">
               First name
             </label>
             <div className="mt-2.5">
               <input
                 type="text"
-                name="first-name"
-                id="first-name"
+                name="firstName"
+                id="firstName"
                 autoComplete="given-name"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+                onChange={handleInputChange}
+             />
             </div>
           </div>
           <div>
@@ -52,10 +115,11 @@ export default function ContactForm() {
             <div className="mt-2.5">
               <input
                 type="text"
-                name="last-name"
-                id="last-name"
+                name="lastName"
+                id="lastName"
                 autoComplete="family-name"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -70,6 +134,7 @@ export default function ContactForm() {
                 id="company"
                 autoComplete="organization"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -84,7 +149,8 @@ export default function ContactForm() {
                 id="email"
                 autoComplete="email"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+                onChange={handleInputChange}
+             />
             </div>
           </div>
           <div className="sm:col-span-2">
@@ -100,6 +166,7 @@ export default function ContactForm() {
                   id="country"
                   name="country"
                   className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                  onChange={handleInputChange}
                 >
                   <option>IND</option>
                   <option>USA</option>
@@ -112,10 +179,11 @@ export default function ContactForm() {
               </div>
               <input
                 type="tel"
-                name="phone-number"
-                id="phone-number"
+                name="phoneNumber"
+                id="phoneNumber"
                 autoComplete="tel"
                 className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -130,6 +198,7 @@ export default function ContactForm() {
                 rows={4}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 defaultValue={''}
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -166,6 +235,7 @@ export default function ContactForm() {
           <button
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={handleSubmit}
           >
             Send
           </button>
