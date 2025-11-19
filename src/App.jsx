b76ed8f6dccac269710bf2dd50794876f2a1a51f
SREEMON KS <sreemonkavungal@gmail.com>
2025-11-19 12:04:12 +0530

    SpeedInsights import code updated in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import PrivacyPage from './pages/PrivacyPage.jsx';
 import NotFound from './pages/NotFound.jsx';
 
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
