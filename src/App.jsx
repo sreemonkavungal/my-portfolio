3857d1b4d0cdf5161e6be86b0b6ac60349799722
SREEMON KS <sreemonkavungal@gmail.com>
2025-11-19 12:02:25 +0530

    installed SpeedInsights and its Component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import PrivacyPage from './pages/PrivacyPage.jsx';
 import NotFound from './pages/NotFound.jsx';
 
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
@@ -27,6 +28,7 @@ function App() {
       <Route path="*" element={<NotFound/>} />
     </Routes>
     <Analytics/>
+    <SpeedInsights />
     </BrowserRouter>
   );
 }
